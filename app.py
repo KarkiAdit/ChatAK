ee95c955cbccaa8fc880fd661b701f8a4f64e967
Aditya Karki <adityakarki728@gmail.com>
2022-01-22 22:09:05 +0545

    Finishing my app.py file

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
@@ -17,3 +18,7 @@ def predict():
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
