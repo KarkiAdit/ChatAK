d5fd1fccbcaaabab044ecb838cdd4be46785c4e2
Aditya Karki <adityakarki728@gmail.com>
2022-01-24 10:22:32 +0545

    Added a minute change

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
+import nltk
+nltk.download('punkt')
 
 app = Flask(__name__)
 CORS(app)
